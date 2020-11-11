28330e956f22868dba0e00946954c1fa6750eab4
shutterstacks <shutterstacks@gmail.com>
2020-11-11 16:54:48 -0500

    stashed cahnges, mostly new fractals

--- model/Fractal.java
+++ model/Fractal.java
@@ -1,16 +1,17 @@
 package FallProject.model;
 
 public class Fractal {
-    public static double radius = 3;
+    public static double radius = 5;
     public static int iterationLimit = 300;
     public static int breakpoint = 50;
     public static int iterate = 0;
     public static double scaling = 0.004;
-    //Scaling value will sometimes not work, idk why. If it happens just change it and hope for the best.
+    //0.004 gives no scale lines
+    //Scaling value will sometimes not work, idk why. If it happens just change add a 1 and hope for the best
     
     public static double k = 0;
-    public static double h = 0.5;
-    public static String name = "Mandelbrot";
+    public static double h = 0;
+    public static String name = "A";
     
     public static void Formula(double x, double y, String type) {
         double outputX, outputY;
@@ -32,6 +33,7 @@ public class Fractal {
         //You can add your extended formula here
         
         switch(type) {
+//Mykyta
             case "Mandelbrot":
                 outputY = initialX*initialX - initialY*initialY + x;
                 outputX = 2*initialX*initialY + y;
@@ -40,11 +42,33 @@ public class Fractal {
                 outputX = initialX*initialX*initialX - 3*initialY*initialX - 1;
                 outputY = 3*initialY*initialX*initialX - initialX;
                 break;
+            case "Cos Blocks":
+                outputX = x/Math.cos(x + initialX);
+                outputY = y/Math.cos(y + initialY);
+                break; 
+            case "Sin Blocks":
+                outputX = x/Math.sin(x + initialX);
+                outputY = y/Math.sin(y + initialY);
+                break;
+            case "Tan Blocks":
+                outputX = x/Math.tan(x + initialX);
+                outputY = y/Math.tan(y + initialY);
+                break;
+            case "A":
+                outputX = x/Math.cos( initialX);
+                outputY = y/Math.cos( initialY);
+                break;
+//// 
+                
+                
+                
+                
             default:
                 outputX = x;
                 outputY = y;
                 break; 
         }
+        //Don't forget break; statement!
 
        temp[0] = (int)Math.floor(outputX/Fractal.scaling);
        temp[1] = (int)Math.floor(outputY/Fractal.scaling);
