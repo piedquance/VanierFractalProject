4b6a2c7a257eeb16adf28a71503ab319b4322b09
Faker-14 <57102573+Faker-14@users.noreply.github.com>
2020-11-11 21:08:45 -0500

    HandleMethods
    Handle Methods for the Fractal ( Julia, Newton, Kock and Mandelbrot

--- model/Fractal.java
+++ model/Fractal.java
@@ -1,6 +1,7 @@
 package FallProject.model;
 
 public class Fractal {
+
     public static double radius = 5;
     public static int iterationLimit = 300;
     public static int breakpoint = 50;
@@ -8,84 +9,76 @@ public class Fractal {
     public static double scaling = 0.004;
     //0.004 gives no scale lines
     //Scaling value will sometimes not work, idk why. If it happens just change add a 1 and hope for the best
-    
+
     public static double k = 0;
     public static double h = 0.5;
     public static String name = "";
-    
+
     public static void Formula(double x, double y, String type) {
         double outputX, outputY;
-        
-        double initialX =  x;  //Deja Vu
+
+        double initialX = x;  //Deja Vu
         double initialY = y;   //I've just been in this place before
-        
-        int[] temp = Plane.getCoord((int)Math.floor(x/Fractal.scaling), (int)Math.floor(y/Fractal.scaling));
-        
-        
-        if(Fractal.iterate != 0) {
-            initialY = temp[0]*scaling + k;
-            initialX = temp[1]*scaling - h;
+
+        int[] temp = Plane.getCoord((int) Math.floor(x / Fractal.scaling), (int) Math.floor(y / Fractal.scaling));
+
+        if (Fractal.iterate != 0) {
+            initialY = temp[0] * scaling + k;
+            initialX = temp[1] * scaling - h;
         } else {
             initialY = y + k;
             initialX = x - h;
         }
-        
+
         //You can add your extended formula here
-        
-        switch(type) {
+        switch (type) {
 //Mykyta
             case "Mandelbrot":
-                outputY = initialX*initialX - initialY*initialY + x;
-                outputX = 2*initialX*initialY + y;
+                outputY = initialX * initialX - initialY * initialY + x;
+                outputX = 2 * initialX * initialY + y;
                 break;
             case "Failed Newton":
-                outputX = initialX*initialX*initialX - 3*initialY*initialX - 1;
-                outputY = 3*initialY*initialX*initialX - initialX;
+                outputX = initialX * initialX * initialX - 3 * initialY * initialX - 1;
+                outputY = 3 * initialY * initialX * initialX - initialX;
                 break;
             case "Cos Blocks":
-                outputX = x/Math.cos(x + initialX);
-                outputY = y/Math.cos(y + initialY);
-                break; 
+                outputX = x / Math.cos(x + initialX);
+                outputY = y / Math.cos(y + initialY);
+                break;
             case "Sin Blocks":
-                outputX = x/Math.sin(x + initialX);
-                outputY = y/Math.sin(y + initialY);
+                outputX = x / Math.sin(x + initialX);
+                outputY = y / Math.sin(y + initialY);
                 break;
             case "Tan Blocks":
-                outputX = x/Math.tan(x + initialX);
-                outputY = y/Math.tan(y + initialY);
+                outputX = x / Math.tan(x + initialX);
+                outputY = y / Math.tan(y + initialY);
                 break;
             case "A":
-                outputX = x/Math.cos( initialX);
-                outputY = y/Math.cos( initialY);
+                outputX = x / Math.cos(initialX);
+                outputY = y / Math.cos(initialY);
+                break;
+            case "Julia":
+                outputX = 0; //just to handle it 
+                outputY = 0;
+
                 break;
 //// 
-                
-                
-                
-                
+
             default:
                 outputX = x;
                 outputY = y;
-                break; 
+                break;
         }
         //Don't forget break; statement!
 
-       temp[0] = (int)Math.floor(outputX/Fractal.scaling);
-       temp[1] = (int)Math.floor(outputY/Fractal.scaling);
+        temp[0] = (int) Math.floor(outputX / Fractal.scaling);
+        temp[1] = (int) Math.floor(outputY / Fractal.scaling);
 
-       if(!FractalRender.CheckCoordOutOfRadius(outputX, outputY))Fractal.iterate++;
-       
-       temp[2] = Fractal.iterate;
+        if (!FractalRender.CheckCoordOutOfRadius(outputX, outputY)) {
+            Fractal.iterate++;
+        }
+
+        temp[2] = Fractal.iterate;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
--- view/controller.java
+++ view/controller.java
@@ -24,68 +24,119 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-
 public class controller implements Initializable {
 
-    @FXML private ImageView image;
-    @FXML private MenuItem Help;
-    @FXML private MenuItem About;
-    
+    @FXML
+    private ImageView image;
+    @FXML
+    private MenuItem Help;
+    @FXML
+    private MenuItem About;
+
     public static Rectangle2D screen = Screen.getPrimary().getBounds();
-    
+
     //added a -100 to be able to view the menu bar.
     public static double screenHeight = screen.getHeight() - 80;
     public static double screenWidth = screen.getWidth();
 
     //public static double screenHeight = 600;
     //public static double screenWidth = 600;
-    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-        
-       image.setFitHeight(screenHeight);
-       image.setFitWidth(screenWidth);
-        
-        WritableImage img = new WritableImage((int)image.getFitWidth(), (int)image.getFitHeight());
-        
+
+        image.setFitHeight(screenHeight);
+        image.setFitWidth(screenWidth);
+
+        WritableImage img = new WritableImage((int) image.getFitWidth(), (int) image.getFitHeight());
+
         PixelWriter writer = img.getPixelWriter();
-   
+
         image.setImage(img);
 
         PixelFormat<ByteBuffer> pixelFormat = PixelFormat.getByteRgbInstance();
-        
+
         byte imageData[] = FractalRender.GetRender();
-       
-        writer.setPixels(0, 0, (int)image.getFitWidth(), (int)image.getFitHeight(), pixelFormat, imageData, 0, (int)image.getFitWidth()*3);
-        
+
+        writer.setPixels(0, 0, (int) image.getFitWidth(), (int) image.getFitHeight(), pixelFormat, imageData, 0, (int) image.getFitWidth() * 3);
+
         image.setImage(img);
 
-    } 
-    
+    }
+
     @FXML
-    private void menuButtons(ActionEvent event) throws Exception{
+    private void menuButtons(ActionEvent event) throws Exception {
         Parent root = new Pane();
-        
+
         String title = " ";
-        if(event.getSource().equals(Help)){
+        if (event.getSource().equals(Help)) {
             root = FXMLLoader.load(getClass().getResource("view/help.fxml"));
             title = "Help";
+        } else if (event.getSource().equals(About)) {
+            title = "About";
         }
-        else if(event.getSource().equals(About)){
-                title = "About";
-        }
-        
+
         Stage secondaryStage = new Stage();
         Scene scene = new Scene(root);
-        
-        
+
         secondaryStage.setTitle(title);
         secondaryStage.setScene(scene);
         secondaryStage.show();
-        
+
     }
+
+    private void printFractal() {
+        WritableImage img = new WritableImage((int) image.getFitWidth(), (int) image.getFitHeight());
+
+        PixelWriter writer = img.getPixelWriter();
+
+        image.setImage(img);
+
+        PixelFormat<ByteBuffer> pixelFormat = PixelFormat.getByteRgbInstance();
+
+        byte imageData[] = FractalRender.GetRender();
+
+        writer.setPixels(0, 0, (int) image.getFitWidth(), (int) image.getFitHeight(), pixelFormat, imageData, 0, (int) image.getFitWidth() * 3);
+
+        image.setImage(img);
+    }
+
+    @FXML
+    private void FractalHandlerJulia(ActionEvent even) {
+
+        Fractal.name = "Julia";
+
+        printFractal();
+
+    }
+
+    @FXML
+    private void FractalHandlerMandelbrot(ActionEvent even) {
+
+        Fractal.name = "Mandelbrot";
+
+        printFractal();
+
+    }
+
+    @FXML
+    private void FractalHandlerNewton(ActionEvent even) {
+
+        Fractal.name = "Newton";
+
+        printFractal();
+
+    }
+    
+    @FXML
+      private void FractalHandlerkock(ActionEvent even) {
+
+        Fractal.name = "Newton";
+
+        printFractal();
+
+    }
+
 }
