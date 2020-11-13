967c2a81cb2b2fbc5c508b9693e1779f3b814d0a
Bappo117 <ExplosiveLemon07@gmail.com>
2020-11-12 19:35:37 -0500

    Big changes and additions. added FXML for all openable windows.
    I added all The FXML files and tried to put them all into the controller. Some Don't open however and it confuses me. Everything in the Info menu works but the Variables category doesn't open anything. All the code is there tho. If anyone wants to mod those files, be my guest :P

--- view/controller.java
+++ view/controller.java
@@ -33,7 +33,22 @@ public class controller implements Initializable {
     private MenuItem Help;
     @FXML
     private MenuItem About;
+    @FXML
+    private MenuItem IterationCount;
+    @FXML
+    private MenuItem Radius;
+    @FXML
+    private MenuItem Position;
+    @FXML
+    private MenuItem Axis;
+    @FXML
+    private MenuItem ColorGradient;
+    @FXML
+    private MenuItem FontSize;
 
+    
+    
+    
     public static Rectangle2D screen = Screen.getPrimary().getBounds();
 
     //added a -100 to be able to view the menu bar.
@@ -71,13 +86,47 @@ public class controller implements Initializable {
     private void menuButtons(ActionEvent event) throws Exception {
         Parent root = new Pane();
 
+
         String title = " ";
         if (event.getSource().equals(Help)) {
             root = FXMLLoader.load(getClass().getResource("help.fxml"));
+//            scene.getStylesheets().add(getClass().getResource("help.css").toExternalForm());
             title = "Help";
             
         } else if (event.getSource().equals(About)) {
+            root = FXMLLoader.load(getClass().getResource("about.fxml"));
+//            scene.getStylesheets().add(getClass().getResource("about.css").toExternalForm());
             title = "About";
+        }
+        else if (event.getSource().equals(Axis)) {
+            root = FXMLLoader.load(getClass().getResource("axis.fxml"));
+//            scene.getStylesheets().add(getClass().getResource("axis.css").toExternalForm());
+            title = "Axis";
+        }
+        else if (event.getSource().equals(IterationCount)) {
+            root = FXMLLoader.load(getClass().getResource("iterationCount.fxml"));
+//            scene.getStylesheets().add(getClass().getResource("iterationcount.css").toExternalForm());
+            title = "Iteration Count";
+        }
+        else if (event.getSource().equals(Radius)) {
+            root = FXMLLoader.load(getClass().getResource("radius.fxml"));
+//            scene.getStylesheets().add(getClass().getResource("radius.css").toExternalForm());
+            title = "Radius";
+        }
+        else if (event.getSource().equals(Position)) {
+            root = FXMLLoader.load(getClass().getResource("position.fxml"));
+//            scene.getStylesheets().add(getClass().getResource("position.css").toExternalForm());
+            title = "Position";
+        }
+        else if (event.getSource().equals(ColorGradient)) {
+            root = FXMLLoader.load(getClass().getResource("colorGradient.fxml"));
+//            scene.getStylesheets().add(getClass().getResource("colorgradient.css").toExternalForm());
+            title = "Color Gradient";
+        }
+        else if (event.getSource().equals(FontSize)) {
+            root = FXMLLoader.load(getClass().getResource("fontSize.fxml"));
+//            scene.getStylesheets().add(getClass().getResource("fontsize.css").toExternalForm());
+            title = "Font Size";
         }
         
         Scene scene = new Scene(root);
@@ -85,8 +134,9 @@ public class controller implements Initializable {
         secondaryStage.setTitle(title);
         secondaryStage.setScene(scene);
         secondaryStage.show();
-
     }
+    
+    
 
     private void printFractal() {
         WritableImage img = new WritableImage((int) image.getFitWidth(), (int) image.getFitHeight());
